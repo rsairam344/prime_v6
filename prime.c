a5d88f8afd403f5a1754fc1b63652a409216df3a
unknown <rsairam344@gmail.com>
2020-02-17 16:06:37 +0530

    first commit

---
+++ prime.c
@@ -0,0 +1,22 @@
+/* File prime.c ---------------------------------------------- */
+/* This example demonstrates some elementary CTC++ properties. */
+/* Not meant to be any good solution to the 'prime' problem.   */
+
+#include "io.h"
+#include "calc.h"
+
+int main(void)
+{
+    unsigned prime_candidate;
+	unsigned prime_candidate1;
+	unsigned prime_candidate2;
+
+    while((prime_candidate = io_ask()) > 0)
+    {
+        if (is_prime(prime_candidate))
+            io_report(prime_candidate, "IS a prime.");
+        else
+            io_report(prime_candidate, "IS NOT a prime.");
+    }
+    return 0;
+}
